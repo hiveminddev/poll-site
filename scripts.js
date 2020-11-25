866fe9316aab4bb6af6d64824844ce8fbb243a8d
HivemindDev <matsuojas@gmail.com>
2020-11-24 23:39:32 -0800

    ajax working

---
+++ scripts.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+    let gifURL = "./assets/check2.gif?v=";
+    console.log( "ready!" );
+    $("#submit-button").click(function(){
+        let word = $("#poll-input").val()
+        console.log(word)
+        word = word.toLowerCase();
+        word = word.trim()
+        const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+        word = word.replace(punctuation,'')
+        $("#poll-input").prop('disabled', true);
+        let message = {
+            "flag":"poll-ans",
+            "payload": word
+        }
+        var res = $.ajax({
+            type:"POST",
+            url:"https://c6qsh7k1l1.execute-api.us-east-2.amazonaws.com/default/hiveMind",
+            data:JSON.stringify(message)
+        })    
+        .done(function(){
+            console.log("success")
+            $("#check").css('background-image','url(' + gifURL + Math.random() + ')');
+            $("#check").show()
+            setTimeout(function(){ 
+                $("#check").fadeOut();
+             }, 5000);
+        })
+        .fail(function() {
+            alert( "error" );
+            $("#poll-input").prop('disabled', false);
+            $("#poll-input").val("")
+        })
+        setTimeout(function(){ 
+            $("#poll-input").prop('disabled', false);
+            $("#poll-input").val("")
+         }, 30000);
+        console.log("res", res)
+    })
+});
